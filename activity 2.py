8c5a61a486e9a07a4db978c5c247a3ec815dee0a
siddharth0212-hash <sidsuperkid02@gmail.com>
2024-12-10 15:46:17 +0300

    activity 2.py

---
+++ activity 2.py
@@ -0,0 +1,30 @@
+# Program to find the number of bits needed to be swapped to make 2 numbers equal
+
+def  totalFlips(number1, number2):
+     
+     #Variable to count flips required
+     flips = 0
+
+     # Get the last bit of both numbers and check of both are same if yes no flip required else flip is required
+     while(number1 > 0 or number 2 > 0):
+           t1 = (number 1 & 1)
+           t2 = (number 2 & 1)
+           if(t1 != t2):
+               flips +=1
+
+            # Right shift both numbers
+           number1>>=1
+           number2>>=1
+
+     return flips
+
+number1 = int(input("Enter First number : "))
+number2 = int(input("Enter second number : "))
+
+print("\nNumber of flips needed :",totalFlips(number1,number2))
+
+    
+
+         
+
+
