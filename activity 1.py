ef399d35dcea20e212982081eb822719c760f2be
siddharth0212-hash <sidsuperkid02@gmail.com>
2024-12-10 15:31:26 +0300

    activity 1

---
+++ activity 1.py
@@ -0,0 +1,27 @@
+# Program to find power set of a set
+
+import math;
+
+def printPowerSet(set,SetSize):
+
+    #Find total elements possible in the power set
+    PowerSetSize = (int) (math.pow(2, SetSize));            
+    outer = 0;
+    inner = 0;
+
+    for outer in range(0, PowerSetSize):
+        for inner in range(0, SetSize):
+            #Check if inner bit in the outer is set If set then print inner element from set
+            if((outer &(1 << inner)) > 0):
+                print(set[inner], end =" ")
+        print("")
+
+size = int(input("Enter array size : "))
+
+set = []
+for i in range(0,size):
+    n = int(input("Enter Element :"))
+    set.append(n)
+
+printPowerSet(set, len(set))
+
